44abd32af77fca0d2001462e019673f00d60c211
Sammy-157 <samuelamankwah157@gmail.com>
2025-05-15 00:09:04 +0000

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "../Styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar-container">
+      <div className="nav-left">
+        <div className="ahoma-image">
+        <img src="src/assets/Ahoma logo.png" alt="ahoma-logo"></img>
+      </div>
+      </div>
       <ul className="nav-menu">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/AboutUs">AboutUs</NavLink></li>
-        <li><NavLink to="/ReachUs">ReachUs</NavLink></li>
-        <li><NavLink to="/Support">Support</NavLink></li>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/AboutUs">AboutUs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ReachUs">ReachUs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Support">Support</NavLink>
+        </li>
       </ul>
+      <div className="nav-right">
+        <button className="signin-button">Sign in </button>
+        <button className="Started-button">Get Started</button>
+      </div>
     </nav>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "../Styles/Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+  <div className="page-with-image">
+  <div className="background-blur"></div> 
+  <div className="home-contents">
+    <h2>Your Fashion Hub</h2>
+    <h1>Manage Everything Seamlessly</h1>
+    <p>
+      Sign up for free in just a few minutes and unlock powerful tools to
+      organize your
+    </p>
+    <p>
+      customers, track jobs, and grow your fashion business with{" "}
+      <span className="ahoma">Ahoma</span>
+    </p>
+    <button className="Started-button-2">
+    Get Started
+  </button>
+  </div>
+  
+</div>
 
-export default Home
+  );
+};
+
+export default Home;
